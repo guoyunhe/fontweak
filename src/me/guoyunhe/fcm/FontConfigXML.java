64d01407876336bd33f41f140df625f0124aa1f8
Guo Yunhe <guoyunhebrave@gmail.com>
2015-08-05 19:42:54 +0300

    Read basic fontconfig XML information.

--- src/me/guoyunhe/fcm/FontConfigXML.java
+++ src/me/guoyunhe/fcm/FontConfigXML.java
@@ -26,7 +26,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -43,6 +42,22 @@ public class FontConfigXML {
     private Document doc;
     
     private boolean antialias;
+    private boolean hinting;
+    private String hintstyle;
+    private String rgba;
+    
+    private String sans;
+    private String serif;
+    private String mono;
+    private String zhSans;
+    private String zhSerif;
+    private String zhMono;
+    private String jaSans;
+    private String jaSerif;
+    private String jaMono;
+    private String koSans;
+    private String koSerif;
+    private String koMono;
 
     public FontConfigXML() {
         factory = DocumentBuilderFactory.newInstance();
@@ -99,27 +114,140 @@ public class FontConfigXML {
         // Read elements
         NodeList list = doc.getElementsByTagName("match");
         
+        // Debug information
+        System.out.println(list.getLength());
+        
         for (int i = 0; i < list.getLength(); i++) {
+            // Debug information
+            System.out.println(i);
+            
             Element element = (Element) list.item(i);
-            if (element.hasAttribute("target")
-                    && element.getAttribute("target").equals("font")) {
-                System.out.println("render option");
-
-                Element editElement = (Element) element.getElementsByTagName("edit").item(0);
-                String name = editElement.getAttribute("name");
-                
-                // Checkout option value
-                switch(name) {
-                    case "antialias":
-                        this.antialias = parseBool(editElement);
-                        System.out.println("antialias: " + this.antialias);
-                        break;
-                }
-                
+            if (element.hasAttribute("target")) {
+                this.findOption(element);
             } else {
-                System.out.println("font family");
+                this.findPattern(element);
             }
         }
+    }
+    
+    private void findOption(Node node) {
+        if (!node.hasAttributes()) {
+            return;
+        }
+        Element element = (Element)node;
+        Element edit = (Element)element.getElementsByTagName("edit").item(0);
+        switch (edit.getAttribute("name")) {
+            case "rgba":
+                this.rgba = this.parseConst(edit);
+                break;
+            case "hinting":
+                this.hinting = this.parseBool(edit);
+                break;
+            case "hintstyle":
+                this.hintstyle = this.parseConst(edit);
+                break;
+            case "antialias":
+                this.antialias = this.parseBool(edit);
+                break;
+        }
+        
+        // Debug information
+        System.out.println(edit.getAttribute("name"));
+    }
+    
+    /**
+     * Find font family pattern in "match" element.
+     * @param node The "match" XML element to be analyzed.
+     */
+    private void findPattern(Node node) {
+        String testFamily = "";
+        String testLanguage = "";
+        String editFamily = "";
+        
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            if (children.item(i).hasAttributes()) {
+                Element child = (Element) children.item(i);
+                switch (child.getNodeName()) {
+                    case "test":
+                        switch (child.getAttribute("name")) {
+                            case "family":
+                                testFamily = this.parseString(child);
+                                break;
+                            case "lang":
+                                testLanguage = this.parseString(child);
+                                break;
+                        }
+                        break;
+                    case "edit":
+                        if (child.getAttribute("name").equals("family")) {
+                            editFamily = this.parseString(child);
+                        }
+                        break;
+                }
+            }
+        }
+        
+        switch (testFamily) {
+            case "sans-serif":
+            case "sans":
+                switch (testLanguage) {
+                    case "zh":
+                    case "zh-cn":
+                    case "zh-tw":
+                        this.zhSans = editFamily;
+                        break;
+                    case "ja":
+                        this.jaSans = editFamily;
+                        break;
+                    case "ko":
+                        this.koSans = editFamily;
+                        break;
+                    default:
+                        this.sans = editFamily;
+                        break;
+                }
+                break;
+            case "serif":
+                switch (testLanguage) {
+                    case "zh":
+                    case "zh-cn":
+                    case "zh-tw":
+                        this.zhSerif = editFamily;
+                        break;
+                    case "ja":
+                        this.jaSerif = editFamily;
+                        break;
+                    case "ko":
+                        this.koSerif = editFamily;
+                        break;
+                    default:
+                        this.serif = editFamily;
+                        break;
+                }
+                break;
+            case "mono":
+            case "monospace":
+                switch (testLanguage) {
+                    case "zh":
+                    case "zh-cn":
+                    case "zh-tw":
+                        this.zhMono = editFamily;
+                        break;
+                    case "ja":
+                        this.jaMono = editFamily;
+                        break;
+                    case "ko":
+                        this.koMono = editFamily;
+                        break;
+                    default:
+                        this.mono = editFamily;
+                        break;
+                }
+                break;
+        }
+        // Debug information
+        System.out.println(testFamily + " " + testLanguage + ": " + editFamily);
     }
     
     /**
@@ -134,6 +262,33 @@ public class FontConfigXML {
             Node child = children.item(i);
             if(child.getNodeName().equals("bool")) {
                 value = Boolean.parseBoolean(child.getTextContent());
+                break;
+            }
+        }
+        return value;
+    }
+    
+    private String parseConst(Node node) {
+        String value = "";
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node child = children.item(i);
+            if(child.getNodeName().equals("const")) {
+                value = child.getTextContent();
+                break;
+            }
+        }
+        return value;
+    }
+    
+    private String parseString(Node node) {
+        String value = "";
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node child = children.item(i);
+            if(child.getNodeName().equals("string")) {
+                value = child.getTextContent();
+                break;
             }
         }
         return value;
