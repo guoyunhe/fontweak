61c75ee4619468ab2ea93c5cd7c77c9a4b47f43f
Guo Yunhe <guoyunhebrave@gmail.com>
2015-08-17 22:12:05 +0300

    Update version label.

--- src/me/guoyunhe/fcm/MainWindow.java
+++ src/me/guoyunhe/fcm/MainWindow.java
@@ -513,7 +513,7 @@ public class MainWindow extends javax.swing.JFrame {
         appTitlePanel.add(appNameLabel);
 
         appVersionLabel.setFont(new java.awt.Font("SansSerif", 0, 18)); // NOI18N
-        appVersionLabel.setText("0.2.0"); // NOI18N
+        appVersionLabel.setText("0.3.0"); // NOI18N
         appTitlePanel.add(appVersionLabel);
 
         aboutContentPanel.add(appTitlePanel);
